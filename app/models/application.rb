c3e19ea082f1a9d108175b0924ccc0ccbdba7406
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-22 14:03:16 -0400

    Dependent destroy added

--- app/models/application.rb
+++ app/models/application.rb
@@ -6,13 +6,13 @@ class Application < ApplicationRecord
   validates :zip_code, presence: true, numericality: true
   validates :application_status, presence: true
 
-  has_many :pet_applications
-  has_many :pets, through: :pet_applications
+  has_many :pet_applications, dependent: :destroy
+  has_many :pets, through: :pet_applications, dependent: :destroy
 
   def pet_application_status(pet_id)
     pet_applications.find_by(pet_id: pet_id).status
   end
-  # 
+  #
   # def potential_pets
   #   pets.uniq
   # end
